1180d1c47759dbebadc06ef3c5f50d5df07be13b
Michael Ogaye <hogaeymic@gmail.com>
2022-03-05 17:31:33 +0300

    ft: The naming logic  is complete and working correctly

--- script.js
+++ script.js
@@ -2,13 +2,57 @@ let btnSub=document.getElementById('collect');
 btnSub.addEventListener('click', (e)=>{
     e.preventDefault();
     let dob=document.getElementById('dob').value ;
-    let gender= document.forms['details'].gender.value;
+   
     //checking for valid dates
-    if(parseInt(dob.slice(5,7))<12 && parseInt(dob.slice(8,10))<31){
+    if(parseInt(dob.slice(5,7))<=12 && parseInt(dob.slice(8,10))<=31){
 
     let dbt= new Date(dob);
     let dayOfWeek=dbt.getDay();
     console.log(dayOfWeek)
+
+    //assigning the Akan names
+    // Note that the first day is sunday and has an index of 0/ saturday has an index of 6
+    let gender= document.forms['details'].gender.value;
+
+    let akanName='';
+    if(dayOfWeek===0){
+      (gender=='Male') ? akanName='Kwasi': akanName='Akosua'
+    }
+    
+     else if(dayOfWeek===1){
+      (gender=='Male') ? akanName='Kwadwo': akanName='Adwoa'
+    }
+    
+    else if(dayOfWeek===2){
+      (gender=='Male') ? akanName='Kwabena': akanName='Abenaa'
+    }
+    
+    else if(dayOfWeek===3){
+      (gender=='Male') ? akanName='Kwaku': akanName='Akua'
+    }
+    
+    else if(dayOfWeek===4){
+      (gender=='Male') ? akanName='Yaw': akanName='Yaa'
+    }
+    
+    else if(dayOfWeek===5){
+      (gender=='Male') ? akanName='Kofi': akanName='Afua'
+    }
+    
+    else if(dayOfWeek===6){
+      (gender=='Male') ? akanName='Kwame': akanName='Ama'
+    }
+    else{
+        akanName="we were unable to predict your name"
+    }
+    console.log(akanName)
+    let fdback=document.getElementById('feedback');
+    fdback.innerHTML=`<P class="text-success">Wow !your Akan-Name is: <span class="text-danger">${akanName}</span></p>`;
+    document.forms['details'].style.display='none';
+
+
+
+
     }
     else{
         alert("You entered an invalid date or month")
