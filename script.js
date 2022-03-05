20f8b6ca95e916bda887c0f23c24b49d587a41f6
Michael Ogaye <hogaeymic@gmail.com>
2022-03-05 16:32:33 +0300

    ft; if-check to filter out invalid dates

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 let btnSub=document.getElementById('collect');
 btnSub.addEventListener('click', (e)=>{
     e.preventDefault();
+    let dob=document.getElementById('dob').value ;
+    let gender= document.forms['details'].gender.value;
+    //checking for valid dates
+    if(parseInt(dob.slice(5,7))<12 && parseInt(dob.slice(8,10))<31){
+
+    let dbt= new Date(dob);
+    let dayOfWeek=dbt.getDay();
+    console.log(dayOfWeek)
+    }
+    else{
+        alert("You entered an invalid date or month")
+    }
+    
+
+
+
 })
